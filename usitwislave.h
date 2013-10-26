088a7bc2fa7cdf75bee3fef0733c67f0cd12ee9e
Erik Slagter <erik@slagter.name>
2013-10-26 11:03:55 +0200

    Forgot to include header file modifications in commit 777625246e16b466398cd212adc07dd80bef0cbe.

--- usitwislave.h
+++ usitwislave.h
@@ -1,13 +1,17 @@
+#ifndef	_USI_TWI_SLAVE_H_
+#define	_USI_TWI_SLAVE_H_
+
 /*	See LICENSE for Copyright etc. */
 
 #include <stdint.h>
 
-#ifndef	_USI_TWI_SLAVE_H_
-#define	_USI_TWI_SLAVE_H_
+enum
+{
+	USI_TWI_BUFFER_SIZE = 32
+};
 
 void		usi_twi_slave(uint8_t slave_address, uint8_t use_sleep,
-				void (*data_callback)(uint8_t buffer_size,
-				volatile uint8_t input_buffer_length, volatile const uint8_t *input_buffer,
+				void (*data_callback)(volatile uint8_t input_buffer_length, volatile const uint8_t *input_buffer,
 				volatile uint8_t *output_buffer_length, volatile uint8_t *output_buffer),
 				void (*idle_callback)(void));
 
